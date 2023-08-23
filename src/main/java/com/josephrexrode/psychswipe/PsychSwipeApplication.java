a0580c579d2318fabe5d18f56f5752c5e86ac430
jrexrode <jrexrode@me.com>
2023-08-23 17:42:34 -0500

    first commit

---
+++ src/main/java/com/josephrexrode/psychswipe/PsychSwipeApplication.java
@@ -0,0 +1,13 @@
+package com.josephrexrode.psychswipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsychSwipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsychSwipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josephrexrode/psychswipe/PsychSwipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephrexrode.psychswipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsychSwipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
