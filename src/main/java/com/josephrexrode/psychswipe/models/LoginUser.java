bdb8b3369fc303485f560732420a9ba9bdb91ec6
jrexrode <jrexrode@me.com>
2023-08-30 15:25:38 -0500

    added user and loginuser models, basic skeleton

---
+++ src/main/java/com/josephrexrode/psychswipe/models/LoginUser.java
@@ -0,0 +1,27 @@
+package com.josephrexrode.psychswipe.models;
+
+public class LoginUser {
+	
+	private String email;
+	
+	private String password;
+	
+	public LoginUser() {}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ src/main/java/com/josephrexrode/psychswipe/models/User.java
@@ -0,0 +1,67 @@
+package com.josephrexrode.psychswipe.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private String username;
+	
+	private String email;
+	
+	private String password;
+	
+	private String confirm;
+	
+	public User() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getConfirm() {
+		return confirm;
+	}
+
+	public void setConfirm(String confirm) {
+		this.confirm = confirm;
+	}
+}
