b7067a1d605d8b01044b35f3519098402013d815
jrexrode <jrexrode@me.com>
2023-09-11 17:13:02 -0500

    finished general skeleton for profile page & functionality

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -1,10 +1,12 @@
 var editButton = document.querySelector(".btn--edit");
 var updateForm = document.querySelector(".form--update");
+var avatar = document.querySelector(".profile--avatar");
 
 editButton.addEventListener("click", toggleUpdateForm);
 
 function toggleUpdateForm() {
 	updateForm.classList.toggle("visible");
+	avatar.classList.toggle("profile--avatar-mini");
 	
 	if (editButton.innerHTML == "Edit") {
 		editButton.innerHTML = "Cancel";
