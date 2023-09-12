878410ad4f56326a4d3a8eaf775259199f30a334
jrexrode <jrexrode@me.com>
2023-09-12 18:13:26 -0500

    added more qualities to Patient model

--- src/main/java/com/josephrexrode/psychswipe/models/Patient.java
+++ src/main/java/com/josephrexrode/psychswipe/models/Patient.java
@@ -20,6 +20,12 @@ public class Patient {
 	
 	private String insuranceProvider;
 	
+	private Boolean insuranceRequirement;
+	
+	private Boolean openToTelehealth;
+	
+	private String stateOfResidence;
+	
 	public Patient() {}
 
 	public Long getId() {
@@ -44,5 +50,29 @@ public class Patient {
 
 	public void setInsuranceProvider(String insuranceProvider) {
 		this.insuranceProvider = insuranceProvider;
+	}
+
+	public Boolean getInsuranceRequirement() {
+		return insuranceRequirement;
+	}
+
+	public void setInsuranceRequirement(Boolean insuranceRequirement) {
+		this.insuranceRequirement = insuranceRequirement;
+	}
+
+	public Boolean getOpenToTelehealth() {
+		return openToTelehealth;
+	}
+
+	public void setOpenToTelehealth(Boolean openToTelehealth) {
+		this.openToTelehealth = openToTelehealth;
+	}
+
+	public String getStateOfResidence() {
+		return stateOfResidence;
+	}
+
+	public void setStateOfResidence(String stateOfResidence) {
+		this.stateOfResidence = stateOfResidence;
 	}
 }
