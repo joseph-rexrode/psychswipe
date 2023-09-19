10a8249a425a0a98c953cb1004ed6b19f88315e2
jrexrode <jrexrode@me.com>
2023-09-19 16:50:36 -0500

    continued working on swiping functionality - now recognizes match or pass based on button pressed or distance traveled

--- src/main/resources/static/js/home.js
+++ src/main/resources/static/js/home.js
@@ -1,11 +1,25 @@
-var card = document.querySelector(".card.swipeCard");
+var cards = document.querySelectorAll(".card.swipeCard");
 
-dragCard(card);
+
+cards.forEach((card) => {
+	dragCard(card);
+})
 
 function dragCard(c) {
 	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0, originX = 0, originY = 0;
 	
 	c.onmousedown = dragMouseDown
+	
+	let passButton = c.querySelector(".btn--pass");
+	let matchButton = c.querySelector(".btn--match");
+	
+	passButton.addEventListener("click", function() {
+		console.log("Pass :(");
+	})
+	
+	matchButton.addEventListener("click", function() {
+		console.log("Match :)");
+	})
 
 	function dragMouseDown(e) {
 		e = e || window.event;
@@ -15,9 +29,11 @@ function dragCard(c) {
 		originY = e.clientY;
 		
 		console.log(originX + ", " + originY);
+		console.log(pos1);
 		
 		pos3 = e.clientX;
 		pos4 = e.clientY;
+		
 		document.onmouseup = closeDragCard;
 		document.onmousemove = cardDrag;
 	}
@@ -31,24 +47,49 @@ function dragCard(c) {
 		pos3 = e.clientX;
 		pos4 = e.clientY;
 		
+		if (pos3 - originX >= 100) {
+			c.style.transform = "rotate(20deg)";
+		}
+		
+		else if (originX - pos3 >= 100) {
+			c.style.transform = "rotate(-20deg)";
+		}
+		
+		else {
+			c.style.transform = "none";
+		}
+		
+		var frOpacity = Math.abs(pos3 - originX) / 100;
+		
+		let cardOpacity = (frOpacity >= 0.8) ? 0.6 : 1 - (frOpacity / 2);
+
+		c.style.opacity = cardOpacity;
+		c.style.position = "absolute";
 		c.style.top = (c.offsetTop - pos2) + "px";
 		c.style.left = (c.offsetLeft - pos1) + "px";
 	}
 	
 	function closeDragCard() {
 		
+		c.style.position = "static";
+		c.style.top = "auto";
+		c.style.left = "auto";
+		
 		if (pos3 - originX >= 100) {
 			c.style.alignSelf = "flex-end";
+			matchButton.click();
+
 		}
 		else if (originX - pos3 > 100) {
 			c.style.alignSelf = "flex-start";
+			passButton.click();
 		}
 		
 		else {
 			c.style.alignSelf = "center";
+			c.style.opacity = "1";
 		}
 		
-		console.log(c.style.alignSelf + ", " + c);
 		
 		document.onmouseup = null;
 		document.onmousemove = null;
